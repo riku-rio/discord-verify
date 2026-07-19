d753187a09d450f3d4f28c6a36c9f00cd8f69626
Yosef Ahmed <YosefBesn123@gmail.com>
2026-07-19 20:40:35 +0300

    feat(cli): implement root command dispatcher

---
+++ packages/cli/src/run-cli.ts
@@ -0,0 +1,37 @@
+import { writeHelp } from './commands/help.js';
+import { registerCommands } from './commands/register-commands.js';
+import { getPackageVersion } from './package-metadata.js';
+
+import type { RunCliOptions } from './cli/types.js';
+
+const HELP_FLAGS = new Set(['-h', '--help']);
+const VERSION_FLAGS = new Set(['-V', '--version']);
+
+export async function runCli(options: RunCliOptions): Promise<number> {
+  const registry = registerCommands();
+  const [commandName, ...args] = options.argv;
+
+  if (commandName === undefined || HELP_FLAGS.has(commandName)) {
+    writeHelp(registry, options.stdout);
+    return 0;
+  }
+
+  if (VERSION_FLAGS.has(commandName)) {
+    options.stdout.write(`${getPackageVersion()}\n`);
+    return 0;
+  }
+
+  const command = registry.get(commandName);
+
+  if (command === undefined) {
+    options.stderr.write(`Unknown command: ${commandName}\n`);
+    options.stderr.write("Run 'discord-verify --help' for usage.\n");
+    return 1;
+  }
+
+  return command.execute({
+    args,
+    stdout: options.stdout,
+    stderr: options.stderr,
+  });
+}
